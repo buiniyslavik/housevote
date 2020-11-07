8fdfa0cedcc9a9717aa018a39563bf791b14237f
Sviatoslav Osin <svosin@zergface.icu>
2020-11-07 13:27:15 +0300

    Initial commit

---
+++ src/test/java/ru/kwuh/housevote/HousevoteApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kwuh.housevote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousevoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
