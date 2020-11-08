01a364e4934637638cdd6107dedc6fd45e3e0072
Sviatoslav Osin <svosin@zergface.icu>
2020-11-08 13:22:28 +0300

    controller for getting currently active votes

--- src/main/java/ru/kwuh/housevote/controllers/VotingController.java
+++ src/main/java/ru/kwuh/housevote/controllers/VotingController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 import ru.kwuh.housevote.entities.Vote;
 import ru.kwuh.housevote.repository.VoteRepository;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(path="/voting", produces = "application/json")
 public class VotingController {
@@ -17,8 +19,16 @@ public class VotingController {
     VoteRepository voteRepository;
 
     @GetMapping("/all")
-    public Iterable<Vote> showCurrentVoting() {
-        PageRequest page = PageRequest.of(0, 12, Sort.by("postingDate").descending());
+    public Iterable<Vote> showAllVoting() {
+        PageRequest page = PageRequest.of(0, 25, Sort.by("postingDate").descending());
         return voteRepository.findAll(page).getContent();
+    }
+
+    @GetMapping("/current")
+    public Iterable<Vote> ShowCurrentVoting() {
+        PageRequest page = PageRequest.of(0, 25, Sort.by("postingDate").descending());
+        List<Vote> voteList = voteRepository.findAll(page).getContent();
+        voteList.removeIf(v -> !v.isCurrentlyUsed()); // remove inactive votes
+        return voteList;
     }
 }
