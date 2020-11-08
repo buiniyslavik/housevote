2b11cc214127c9384f49d562b9365bc4f38ad667
Sviatoslav Osin <svosin@zergface.icu>
2020-11-08 17:05:51 +0300

    figuring out JSON

--- src/main/java/ru/kwuh/housevote/entities/Question.java
+++ src/main/java/ru/kwuh/housevote/entities/Question.java
@@ -1,10 +1,12 @@
 package ru.kwuh.housevote.entities;
 
+import com.fasterxml.jackson.annotation.JsonRootName;
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
 
 @Data
+@JsonRootName("question")
 public class Question {
     @NotNull
     final String questionText;
