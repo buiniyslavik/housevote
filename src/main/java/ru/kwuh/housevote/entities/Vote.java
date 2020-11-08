307e1e705cb9113811212c156512073b0d592ca3
Sviatoslav Osin <svosin@zergface.icu>
2020-11-08 17:05:51 +0300

    figuring out JSON

--- src/main/java/ru/kwuh/housevote/entities/Vote.java
+++ src/main/java/ru/kwuh/housevote/entities/Vote.java
@@ -1,9 +1,7 @@
 package ru.kwuh.housevote.entities;
 
-import lombok.Data;
-import lombok.Generated;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonRootName;
+import lombok.*;
 import org.springframework.data.annotation.PersistenceConstructor;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -13,41 +11,54 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 // Описывает собрание собственников
 @Document
 @Data
-
+@JsonRootName("vote")
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Vote {
     @Id //@GeneratedValue(strategy = GenerationType.AUTO)
     Long voteId;
     @NotNull @Indexed
-    final Long houseId; // дом, в котором проходит голосование
+    Long houseId; // дом, в котором проходит голосование
     @NotNull
-    final LocalDateTime postingDate; // дата создания голосования
-    final LocalDateTime voteStartDate;
-    final LocalDateTime voteEndDate;
+    LocalDateTime postingDate; // дата создания голосования
+    LocalDateTime voteStartDate;
+    LocalDateTime voteEndDate;
     List<Question> questionList;
+ //   String question;
     List<User> onlineParticipants;
     List<OfflineVoter> offlineVoters;
 
     boolean isCurrentlyUsed = false;
-
+/*
     public Vote(Long houseId) {
         this.houseId = houseId;
         postingDate = LocalDateTime.now();
         voteStartDate = postingDate.plusDays(7).withHour(0).withMinute(0).withSecond(0).withNano(0);
         voteEndDate = voteStartDate.plusHours(24);
     }
+    public Vote(Long houseId, String question) {
+        this.houseId = houseId;
+        this.questionList = new ArrayList<>();
+        this.questionList.add(new Question(question, false));
+        postingDate = LocalDateTime.now();
+        voteStartDate = postingDate.plusDays(7).withHour(0).withMinute(0).withSecond(0).withNano(0);
+        voteEndDate = voteStartDate.plusHours(24);
+    } */
+
     @PersistenceConstructor
     public Vote(Long houseId, LocalDateTime postingDate, LocalDateTime voteStartDate,
-                LocalDateTime voteEndDate, boolean isCurrentlyUsed) {
+                LocalDateTime voteEndDate, boolean isCurrentlyUsed, List<Question> questionList) {
         this.houseId = houseId;
         this.postingDate = postingDate;
         this.voteStartDate = voteStartDate;
         this.voteEndDate = voteEndDate;
         this.isCurrentlyUsed = isCurrentlyUsed;
+        this.questionList = questionList;
     }
     public void addQuestion(String question, boolean needsTwoThirds) {
         questionList.add(new Question(question, needsTwoThirds));
