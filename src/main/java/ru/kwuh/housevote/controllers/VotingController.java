985571b2f15c165cc83fda7d96963095ffe7f3bd
Sviatoslav Osin <svosin@zergface.icu>
2020-11-10 14:20:56 +0300

    voting now -kinda- works

--- src/main/java/ru/kwuh/housevote/controllers/VotingController.java
+++ src/main/java/ru/kwuh/housevote/controllers/VotingController.java
@@ -5,13 +5,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
-import ru.kwuh.housevote.entities.Question;
-import ru.kwuh.housevote.entities.Vote;
+import ru.kwuh.housevote.entities.*;
+import ru.kwuh.housevote.repository.HouseRepository;
 import ru.kwuh.housevote.repository.VoteRepository;
 
 import javax.validation.Valid;
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
@@ -23,6 +25,9 @@ public class VotingController {
 
     @Autowired
     VoteRepository voteRepository;
+
+    @Autowired
+    HouseRepository houseRepository;
 
     @GetMapping(value = {"/all", "/all/{page}"})
     public Iterable<Vote> showAllVoting(@PathVariable(name = "page", required = false) Integer pageNumber) {
@@ -50,15 +55,50 @@ public class VotingController {
 
     @PostMapping(value = "/add", consumes = "application/json")
     public Vote addNewVoting(@RequestBody @Valid Vote vote) {
-        return voteRepository.save(vote);
+        List<OnlineVoter> registeredHouseVoters = new ArrayList<>();
+        if(houseRepository.findById(vote.getHouseId()).isPresent()) {
+            House houseOnVote = houseRepository.findById(vote.getHouseId()).get();
+            houseOnVote.getRegisteredUsers().forEach(user -> {
+                registeredHouseVoters.add(new OnlineVoter(user.getId()));
+            });
+            vote.setOnlineParticipants(registeredHouseVoters);
+            return voteRepository.save(vote);
+        }
+        return null;
     }
 
     // -------------------------------------------------
+
 
     @GetMapping("/{voteId}")
     public Iterable<Question> showVoteQuestions(@PathVariable(name = "voteId") BigInteger voteId) {
         if (voteRepository.findById(voteId).isPresent())
             return voteRepository.findById(voteId).get().getQuestionList();
         else return null;
+    }
+
+    @PutMapping(value = "/{voteId}", consumes = "application/json")
+    public Iterable<Response> respondToQuestion(
+            @PathVariable(name="voteId") BigInteger voteId,
+            @RequestBody Response response
+    ) {
+        Vote vote;
+        if (voteRepository.findById(voteId).isPresent())
+            vote = voteRepository.findById(voteId).get();
+        else return null;
+        OnlineVoter currentVoter;
+        try {
+                    currentVoter = vote.getOnlineParticipants()
+                    .stream()
+                    .filter(onlineVoter ->
+                            onlineVoter.getUserId().equals(response.getUserId()))
+                    .findFirst().get(); //TODO make it less horrible
+        }
+        catch(NoSuchElementException nsex) {
+            return null;
+        }
+        currentVoter.getResponses().add(response);
+        voteRepository.save(vote);
+        return currentVoter.getResponses();
     }
 }
