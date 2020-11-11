4628e9eaa57fb2edd98b7ddd811dbea702c68dd8
Sviatoslav Osin <svosin@zergface.icu>
2020-11-11 15:19:17 +0300

    "about me" and "my property" endpoint

--- src/main/java/ru/kwuh/housevote/controllers/ProfileController.java
+++ src/main/java/ru/kwuh/housevote/controllers/ProfileController.java
@@ -3,19 +3,27 @@ package ru.kwuh.housevote.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+import ru.kwuh.housevote.entities.House;
 import ru.kwuh.housevote.entities.Profile;
 import ru.kwuh.housevote.entities.SignupRequest;
+import ru.kwuh.housevote.repository.HouseRepository;
 import ru.kwuh.housevote.repository.ProfileRepository;
 
 import javax.validation.Valid;
+import java.math.BigInteger;
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/profile", produces = "application/json")
 public class ProfileController {
     @Autowired
     ProfileRepository profileRepository;
+
+    @Autowired
+    HouseRepository houseRepository;
 
     @PostMapping(path = "/create", consumes = "application/json")
     public Profile createNewProfile(@RequestBody @Valid SignupRequest signupRequest) {
@@ -41,5 +49,17 @@ public class ProfileController {
         else
             page = PageRequest.of(0, 25, Sort.by("postingDate").descending());
         return profileRepository.findAll(page).getContent();
+    }
+
+    @GetMapping("/me")
+    public Profile getMyInfo() {
+        String currentEmail = SecurityContextHolder.getContext().getAuthentication().getName();
+        return profileRepository.findUserByEmailAddress(currentEmail);
+    }
+    @GetMapping("/me/property")
+    public Iterable<House> getMyProperty() {
+        String currentEmail = SecurityContextHolder.getContext().getAuthentication().getName();
+        List<BigInteger> propIds = profileRepository.findUserByEmailAddress(currentEmail).getOwnedProperty();
+        return houseRepository.findAllById(propIds);
     }
 }
