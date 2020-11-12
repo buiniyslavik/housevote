a9fd402157ca3816d806548b0a879b465f0fc852
Dmitry Sharonov <dmitry_sharonov@epam.com>
2020-11-12 17:40:21 +0300

    front: fix fail

--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -1,5 +1,10 @@
-//const addVoteConnection = 'http://127.0.0.1:8080/voting/all';
-const addVoteConnection = 'api/voting/all';
+//const server = 'http://127.0.0.1:8080';
+const server = 'api';
+const votingPage = '/voting';
+const allVoteConnection = server + votingPage + '/all';
+const addAnswerConnection = server + votingPage + '/id/';
+
+
 function addVoteRedirect(){
     window.location.href = "addVote.html";
 }
@@ -10,19 +15,16 @@ function scrollChat(){
 }
 
 window.onload = function () {
-    Vue.component('question-item', {
-        props: ['question'],
-        template: 
-        '<li>'+
-            '<div class="question">'+
-                '<p class="question_text">{{ question.questionList[0].questionText }}</p>'+
-                '<button class="button_block">Да</button>'+
-                '<button class="button_block">Нет</button>'+
-                '<button class="button_block">Воздержусь</button>'+
-            '</div>'+
-        '</li>'
-      });
+    getUserId();
+    getQuestions();
+    scrollChat();
+}
 
+function getUserId(){
+    axios.get(server + "/profile/me").then(response =>{profile = response.data.id});
+}
+
+function getQuestions(){
     var questionList = new Vue({
         el: '.questions',
         data() {
@@ -31,9 +33,39 @@ window.onload = function () {
             };
         },
         mounted(){
-            axios.get(addVoteConnection).then(response =>(this.questionList = response.data));
+            axios.get(allVoteConnection).then(response =>(this.questionList = response.data));
+        }
+        
+    });
+    Vue.component('question-item', {
+        props: ['question'],
+        template: 
+        '<li>'+
+            '<div class="question">'+
+                '<p class="question_text">{{ question.questionList[0].questionText }}</p>'+
+                `<button class="button_block" v-on:click="addAnswer('YES', question.id)">Да</button>`+
+                `<button class="button_block" v-on:click="addAnswer('NO', question.id)">Нет</button>`+
+                `<button class="button_block" v-on:click="addAnswer('ABSTAINED', question.id)">Воздержусь</button>`+
+            '</div>'+
+        '</li>',
+        methods:{
+            addAnswer: function(answer, questionId){
+                if(profile == null){
+                    alert('Вы не авторизованны');
+                }else{
+                    axios.put(addAnswerConnection + questionId, {
+                        "profileId": profile,
+                        "questionNumber": 0,
+                        "answer": answer
+                    });
+                } 
+            }
         }
     });
-
-    scrollChat();
+    /*
+    `<button class="button_block" v-on:click="addAnswer('YES', question.id)">Да</button>`+
+                `<button class="button_block" v-on:click="addAnswer('NO', question.id)">Нет</button>`+
+                `<button class="button_block" v-on:click="addAnswer('ABSTAINED', question.id)">Воздержусь</button>`+
+    */
+   
 }
