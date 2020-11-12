24527c7d2ef7cf970ba6d56356432c273db76769
Dmitry Sharonov <dmitry_sharonov@epam.com>
2020-11-12 22:13:14 +0300

    front: add chat

--- frontend/js/chat.js
+++ frontend/js/chat.js
@@ -1,3 +1,21 @@
-function sendMesagge(id){
+function sendMessage(){
+    var name = "Valeriy Ryzhaev";
+    var date = new Date();
+    var dateString = date.getDate() + '.' + (date.getMonth() + 1) + '.' + date.getFullYear();
+    var text = document.getElementById("message").value;
+    message=
+    '<div class="message">'+
+        '<div class="message_author">'+name+'</div>'+
+        '<div class="message_date_time">'+dateString+'</div>'+
+        '<div class="message_text">'+text+'</div>'+
+    '</div>';
+
+    var li = document.createElement("li");
+    li.innerHTML = message;
+    var ul = document.getElementById("chatList");
+    ul.appendChild(li);
+    var text = document.getElementById("message");
+    text.value = "";
     
+    scrollChat();
 }
