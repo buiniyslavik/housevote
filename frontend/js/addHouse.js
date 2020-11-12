3f228fd99dafbc4670dcb7f2a0023326033159a2
Vaness000 <rayzhaev99@mail.ru>
2020-11-12 21:39:05 +0300

    changed addHouse js

--- frontend/js/addHouse.js
+++ frontend/js/addHouse.js
@@ -1,19 +1,15 @@
-//const addVoteConnection = 'api/house/add';
-//function addVote(){    
-//    axios.post(addVoteConnection, {
-//        "houseId": document.getElementById("vote_house").value,
-//        "questionList": 
-//        [
-//            {
-//                "questionText": document.getElementById("vote_message").value,
-//                "needsTwoThirds": false
-//            }
-//        ] 
-//    });  
-//    document.getElementById("done").innerHTML="Добавлено!";
-//   setTimeout("window.location.href = 'index.html'", 1000);
-//} -- wtf??
 function addHouse(){
+    axios.post(addHouseConnection, {
+        "address": document.getElementById("NHouseAddress").value,
+    }).catch(function(error){
+        if(error.response.status == 404){
+            document.getElementById("errorArea").innerHTML += "\n" +"[404], либо серв не запущен, либо надо поменять ip серва";
+        }if(error.response.status == 401){
+            document.getElementById("errorArea").innerHTML += "\n" +"[401], Авторизуйся";
+        }else{
+            document.getElementById("errorArea").innerHTML += error;
+        }
+    });
     document.getElementById("done").innerHTML="Добавлено!";
     setTimeout("window.location.href = 'index.html'", 1000);
 }
